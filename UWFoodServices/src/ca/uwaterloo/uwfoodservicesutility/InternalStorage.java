d273f4f507d7aa9877201dd6242d170e196a7658
Shamak Dutta <shamakd5@gmail.com>
2013-07-30 00:14:26 -0400

    Issue #11. App will always try to load data from cache regardless of whether there is a network or not. It checks whether the week when the data was cached is the same as the current week. If they are not the same, it will display a toast for now. Requires a lot of testing before the issue can be closed.

--- UWFoodServices/src/ca/uwaterloo/uwfoodservicesutility/InternalStorage.java
+++ UWFoodServices/src/ca/uwaterloo/uwfoodservicesutility/InternalStorage.java
@@ -15,6 +15,16 @@ public class InternalStorage{
 	   private InternalStorage() {
 		   
 	   }
+	   
+	   public static boolean cacheExists(Context context, String key) throws IOException, ClassNotFoundException{
+		   
+		   File file = new File(context.getFilesDir(), key);
+		   if(!file.isFile() && !file.canRead()){
+			   return false; 
+		   }
+		   else return true;
+		      
+	   }
 	 
 	   public static void writeObject(Context context, String key, Object object) throws IOException {
 	      FileOutputStream fileOutputStream = context.openFileOutput(key, Context.MODE_PRIVATE);
@@ -26,17 +36,14 @@ public class InternalStorage{
 	 
 	   public static Object readObject(Context context, String key) throws IOException,
 	         ClassNotFoundException {
-		  File file = new File(context.getFilesDir(), key);
-	      if(!file.isFile() && !file.canRead()){
-	    	  Log.d("File", "No such File");
-	    	  return null; 
-	      }
-	      else{
+		   if(cacheExists(context, key)){
 	    	  FileInputStream fileInputStream = context.openFileInput(key);
 		      ObjectInputStream objectInputSteam = new ObjectInputStream(fileInputStream);
 		      Object object = objectInputSteam.readObject();
 		      return object;
 	      }
+		  
+		   else return null;
 	   }
 	   
 	   public static boolean deleteObject(Context context, String key) {
