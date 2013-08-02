714d7784d6c0bbdfa2be70ac0b8ddbc90de8a21b
Shamak Dutta <shamakd5@gmail.com>
2013-08-01 21:33:14 -0400

    Removed previous MainScreen code. Sliding menu now points to the new main menu

--- UWFoodServices/src/ca/uwaterloo/uwfoodservices/MainScreen.java
+++ UWFoodServices/src/ca/uwaterloo/uwfoodservices/MainScreen.java
@@ -1,62 +1,100 @@
 package ca.uwaterloo.uwfoodservices;
 
+import ca.uwaterloo.mainscreencontrols.*;
+import ca.uwaterloo.mainscreencontrols.SpinningMenuAdapter.OnItemClickListener;
+import ca.uwaterloo.mainscreencontrols.SpinningMenuAdapter.OnItemSelectedListener;
+
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
+import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainScreen extends Activity {
+	
+	private int requestCode = -1;
+	private String selectedTab;
 
-    private int requestCode = -1;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+	    getActionBar().hide();
+		setContentView(R.layout.activity_main_screen);
+		
+		final Intent intent_restaurant = new Intent(this, RestaurantMenuList.class);
+		final Intent intent_location = new Intent(this, LocationHours.class);
+		final Intent intent_settings = new Intent(this, SettingsActivity.class);
+		
+		SpinningMenu SpinningMenu = (SpinningMenu)findViewById(R.id.SpinningMenu);
+        SpinningMenu.setOnItemClickListener(new OnItemClickListener(){
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+			public void onItemClick(SpinningMenuAdapter<?> parent, View view,
+					int position, long id) {	
+				String temp = (String)((SpinningMenuItem) parent.getChildAt(position)).getName();
 
-        ImageView image_menu = (ImageView) findViewById(R.id.menuImage);
-        ImageView image_location = (ImageView) findViewById(R.id.locationImage);	
 
-        final Intent intent_restaurant = new Intent(this, RestaurantMenuList.class);
-        final Intent intent_location = new Intent(this, LocationHours.class);
-
-        Typeface tf = Typeface.createFromAsset(getAssets(),
-                "Roboto-Medium.ttf");
-
-        TextView text_menu = (TextView) findViewById(R.id.menuText);
-        TextView text_location = (TextView) findViewById(R.id.locationText);
-        text_menu.setTypeface(tf);
-        text_location.setTypeface(tf);
-
-        image_menu.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View arg0) {
-                startActivityForResult(intent_restaurant, requestCode);
-            }
-
+					if (temp.equals("Menu")&&selectedTab.equals("Menu"))
+					{
+						startActivityForResult(intent_restaurant, requestCode);
+					}
+					else if (temp.equals("Locations and Hours")&&selectedTab.equals("Locations and Hours"))
+					{
+						startActivityForResult(intent_location, requestCode);
+					}
+					else if (temp.equals("Settings")&&selectedTab.equals("Settings"))
+					{
+						startActivityForResult(intent_settings, requestCode);
+					}
+					else if (temp.equals("About Us")&&selectedTab.equals("About Us"))
+					{
+						
+					}
+				
+				}
+        	
         });
 
-        image_location.setOnClickListener(new OnClickListener(){
+        SpinningMenu.setOnItemSelectedListener(new OnItemSelectedListener(){
 
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(intent_location, requestCode);
-            }
+			public void onItemSelected(SpinningMenuAdapter<?> parent, View view,
+					int position, long id) {
+				
 
-        });
+		        
+				switch(position){
+				case 0:
+					selectedTab = "Menu";
+					break;
+				case 1:
+					selectedTab = "Locations and Hours";
+					break;
+				case 2:
+					selectedTab = "Settings";
+					break;
+				case 3:
+					selectedTab = "Abouts Us";
+					break;
+				}
+				
+			}
 
-    }
+			public void onNothingSelected(SpinningMenuAdapter<?> parent) {
+			}
+        	
+        }
+        );
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_screen, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_screen2, menu);
+		return true;
+	}
 
 }
