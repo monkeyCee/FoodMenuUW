f8846666b6404513173a91825069d7dd151e4111
coreywu <coreywu0@gmail.com>
2013-08-01 21:12:01 -0400

    Changed NetworkReceiver to be a separate class, moved method updateConnectedFlags to be in that class.

--- UWFoodServices/src/ca/uwaterloo/uwfoodservices/SplashScreen.java
+++ UWFoodServices/src/ca/uwaterloo/uwfoodservices/SplashScreen.java
@@ -9,14 +9,12 @@ import java.util.Locale;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -32,6 +30,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import ca.uwaterloo.uwfoodservicesutility.InternalStorage;
+import ca.uwaterloo.uwfoodservicesutility.NetworkReceiver;
 import ca.uwaterloo.uwfoodservicesutility.ParseMenuData;
 import ca.uwaterloo.uwfoodservicesutility.RestaurantMenuHolder;
 import ca.uwaterloo.uwfoodservicesutility.RestaurantMenuObject;
@@ -40,30 +39,24 @@ public class SplashScreen extends Activity {
 
     static ParseLocationData locationParser;
     static ParseMenuData menuParser;
-    SharedPreferences sharedPrefs;
     SharedPreferences.Editor prefEditor;
 
     public static final String WIFI = "Wi-Fi Only";
     public static final String BOTH = "Both Wi-Fi and Data";
-    public static final String DATA = "Data Only";
+    public static final String DATA = "Data Only";    
 
-    private static boolean wifiConnected = false;
-    private static boolean mobileConnected = false;
-    private static boolean dataConnected = false;
-    public static boolean refreshDisplay = true;
-
-    public static String networkPref = null;
+    public String networkPref;
+    SharedPreferences sharedPrefs;
     public static boolean cachePref;
+    public static String refreshPref = null;
     public static int weekStored;
 
-    public static String refreshPref = null;
-    
     public static String formattedDate;
     static int weekDay;
     static Calendar calendar;
     static SimpleDateFormat simpleDateFormat;
 
-    private NetworkReceiver receiver = new NetworkReceiver();
+    private NetworkReceiver receiver;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -71,18 +64,17 @@ public class SplashScreen extends Activity {
 		setContentView(R.layout.activity_splash_screen);	
 
 		IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-		receiver = new NetworkReceiver();
+		receiver = new NetworkReceiver(this);
 		this.registerReceiver(receiver, filter);
 
-        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        networkPref = sharedPrefs.getString("connection_type_preference", "Both Wi-Fi and Data");
+		networkPref = receiver.getNetworkPref();
+		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         cachePref = sharedPrefs.getBoolean("save_data_preference", true);
         weekStored = sharedPrefs.getInt("storedWeek", -1);
         refreshPref = sharedPrefs.getString("refresh", "");
         Log.d(networkPref, "NETWORK PREF 1");
 
-        updateConnectedFlags();
+        receiver.updateConnectedFlags();
 
         loadData();
     }
@@ -208,9 +200,7 @@ public class SplashScreen extends Activity {
             else{
                 Log.d("Object is null", "Null");
             }
-
         }
-
     }
 
     public String getDatedMenuUrl() {
@@ -252,62 +242,6 @@ public class SplashScreen extends Activity {
 
     }
 
-    public class NetworkReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-            // Wifi or Data Connected
-            if (BOTH.equals(networkPref) && (networkInfo != null) && ((networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
-                    || (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE))) {
-                refreshDisplay = true;
-                if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                    Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(context, R.string.data_connected, Toast.LENGTH_SHORT).show();
-                }
-
-                // Wifi Connected
-            } else if (WIFI.equals(networkPref) && (networkInfo != null) && (networkInfo.getType() == ConnectivityManager.TYPE_WIFI)) {
-                refreshDisplay = true;
-                Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
-
-                // Data Connected
-            } else if (DATA.equals(networkPref) && (networkInfo != null) && (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE)) {
-                refreshDisplay = true;
-                Toast.makeText(context, R.string.data_connected, Toast.LENGTH_SHORT).show();
-
-                // Otherwise, the app can't download content--either because there is no network
-                // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
-                // is no Wi-Fi connection.
-                // Sets refreshDisplay to false.
-            } else {
-                refreshDisplay = false;
-                Toast.makeText(context, R.string.lost_connection, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void updateConnectedFlags() {
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
-        if (activeInfo != null) {
-            Log.d(activeInfo.isConnected() + "", "network connected");
-        } else {
-            Log.d(null + "", "network connected");
-        }
-        if ((activeInfo != null) && activeInfo.isConnected()) {
-            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
-            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
-        } else {
-            wifiConnected = false;
-            mobileConnected = false;
-        }
-    }
-
 	@SuppressWarnings("unchecked")
     private void loadData() {
     	
@@ -321,8 +255,8 @@ public class SplashScreen extends Activity {
 		
 		if(refreshPref.equals("locations") || refreshPref.equals("menu")){
 			
-			if (((networkPref.equals(BOTH)) && (wifiConnected || mobileConnected))
-	                || ((networkPref.equals(WIFI)) && (wifiConnected)) || ((networkPref.equals(DATA)) && (mobileConnected))) {
+			if (((networkPref.equals(BOTH)) && (receiver.wifiConnected || receiver.mobileConnected))
+	                || ((networkPref.equals(WIFI)) && (receiver.wifiConnected)) || ((networkPref.equals(DATA)) && (receiver.mobileConnected))) {
 				
 					Toast.makeText(getApplicationContext(), "Refreshing using network", Toast.LENGTH_SHORT).show();
 	        
@@ -367,8 +301,8 @@ public class SplashScreen extends Activity {
 			
 			else{ 
 				Toast.makeText(getApplicationContext(), "Cannot refresh because there is no network", Toast.LENGTH_SHORT).show();
-				showErrorPage(); }	
-			
+				showErrorPage(); 
+			}	
 		}
 		
 		else{
@@ -381,8 +315,8 @@ public class SplashScreen extends Activity {
 				InternalStorage.deleteObject(SplashScreen.this, "location");
 				
 				//If network, get new data else error page				
-				if (((networkPref.equals(BOTH)) && (wifiConnected || mobileConnected))
-		                || ((networkPref.equals(WIFI)) && (wifiConnected)) || ((networkPref.equals(DATA)) && (mobileConnected))) {
+				if (((networkPref.equals(BOTH)) && (receiver.wifiConnected || receiver.mobileConnected))
+		                || ((networkPref.equals(WIFI)) && (receiver.wifiConnected)) || ((networkPref.equals(DATA)) && (receiver.mobileConnected))) {
 					
 						Toast.makeText(getApplicationContext(), "Cache Pref off, Loading from network", Toast.LENGTH_SHORT).show();
 		        
@@ -409,8 +343,8 @@ public class SplashScreen extends Activity {
 	    		try {
 					if(!(InternalStorage.cacheExists(SplashScreen.this, "menu")) || !(InternalStorage.cacheExists(SplashScreen.this, "location"))){
 						
-						if (((networkPref.equals(BOTH)) && (wifiConnected || mobileConnected))
-				                || ((networkPref.equals(WIFI)) && (wifiConnected)) || ((networkPref.equals(DATA)) && (mobileConnected))) {
+						if (((networkPref.equals(BOTH)) && (receiver.wifiConnected || receiver.mobileConnected))
+				                || ((networkPref.equals(WIFI)) && (receiver.wifiConnected)) || ((networkPref.equals(DATA)) && (receiver.mobileConnected))) {
 														
 								Toast.makeText(getApplicationContext(), "Cache Pref On. No Cached data. Getting data from network", Toast.LENGTH_SHORT).show();
 				        
---
+++ UWFoodServices/src/ca/uwaterloo/uwfoodservicesutility/NetworkReceiver.java
@@ -0,0 +1,89 @@
+package ca.uwaterloo.uwfoodservicesutility;
+
+import ca.uwaterloo.uwfoodservices.R;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class NetworkReceiver extends BroadcastReceiver {
+
+    public final String WIFI = "Wi-Fi Only";
+    public final String BOTH = "Both Wi-Fi and Data";
+    public final String DATA = "Data Only";    
+    
+    Context context;
+    
+    public boolean wifiConnected = false;
+    public boolean mobileConnected = false;
+    public boolean dataConnected = false;
+    
+    public String networkPref = null;
+    public boolean cachePref;
+    public String refreshPref = null;
+    SharedPreferences sharedPrefs;
+    
+    public NetworkReceiver(Context context) {
+        this.context = context;
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        networkPref = sharedPrefs.getString("connection_type_preference", "Both Wi-Fi and Data");
+    }
+    
+    public String getNetworkPref() {
+        return networkPref;
+    }
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // Wifi or Data Connected
+        if (BOTH.equals(networkPref) && (networkInfo != null) && ((networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                || (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE))) {
+            if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, R.string.data_connected, Toast.LENGTH_SHORT).show();
+            }
+
+            // Wifi Connected
+        } else if (WIFI.equals(networkPref) && (networkInfo != null) && (networkInfo.getType() == ConnectivityManager.TYPE_WIFI)) {
+            Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
+
+            // Data Connected
+        } else if (DATA.equals(networkPref) && (networkInfo != null) && (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE)) {
+            Toast.makeText(context, R.string.data_connected, Toast.LENGTH_SHORT).show();
+
+            // Otherwise, the app can't download content--either because there is no network
+            // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
+            // is no Wi-Fi connection.
+            // Sets refreshDisplay to false.
+        } else {
+            Toast.makeText(context, R.string.lost_connection, Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+    public void updateConnectedFlags() {
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+        if (activeInfo != null) {
+            Log.d(activeInfo.isConnected() + "", "network connected");
+        } else {
+            Log.d(null + "", "network connected");
+        }
+        if ((activeInfo != null) && activeInfo.isConnected()) {
+            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+        } else {
+            wifiConnected = false;
+            mobileConnected = false;
+        }
+    }
+}
