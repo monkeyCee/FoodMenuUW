933bed34832e7174569996f63c8d6ee42beac48e
Shamak <shamakd5@gmail.com>
2013-08-30 23:17:12 +0400

    Modified the location parser to use urllib2 instead of requests

--- Scripts/Locations/fetchLocation.py
+++ Scripts/Locations/fetchLocation.py
@@ -1,23 +1,19 @@
-import json, requests
+import json, urllib2, urllib
+
+data = json.load(urllib2.urlopen('http://api.uwaterloo.ca/public/v1/?key=4aa5eb25c8cc979600724104ccfb70ea&service=FoodServices&output=json'));
 
 timing = "";
-response = requests.get('http://api.uwaterloo.ca/public/v1/?key=4aa5eb25c8cc979600724104ccfb70ea&service=FoodServices&output=json')
-data = json.loads(response.text)
 result = data['response']['data']['result']
 for location in result:
 	timing = "";
-	print ""
-	print "Location:", location['Location']
-	print "ID:", location['ID']
 	for timings in location['Hours']['result']:
 		timing += timings + "\n";
-	print "Timings:", timing
 	if(location['ID'] == "20" or location['ID'] == "22"):
 		params = {'id': location['ID'], 'location': location['Name'],'timings': timing}
 	else:
 		params = {'id': location['ID'], 'location': location['Location'],'timings': timing}
-	response1= requests.post("http://localhost/create.php",data=params);
-	print response1.text
-
-
-
+	parameters = urllib.urlencode(params);
+	request = urllib2.Request('http://localhost/create.php', parameters);
+	response = urllib2.urlopen(request);	
+  	print response.read();
+	
